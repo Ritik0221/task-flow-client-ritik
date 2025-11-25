6111f29992e95541577e39a487befd1677a90a08
ritik <ritikjesial@gmail.com>
2025-11-25 19:16:00 +0530

    added priority filter

--- src/pages/TaskList.jsx
+++ src/pages/TaskList.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState , useMemo} from 'react'
 import axios from '../api/axiosInstance'
 import TaskCard from '../components/TaskCard'
 import { Link, useSearchParams } from 'react-router-dom'
@@ -6,6 +6,8 @@ import { Link, useSearchParams } from 'react-router-dom'
 export default function TaskList() {
   const [tasks, setTasks] = useState([])
   const [loading, setLoading] = useState(true)
+  //states to manage filters
+  const [Filter, setFilter] = useState('')
 
   // Get search query from URL
   const [searchParams] = useSearchParams()
@@ -43,20 +45,46 @@ export default function TaskList() {
     }
   }
 
+
+  // Filter tasks based on priority
+   const filteredTasks = useMemo(() => {
+    if (Filter === '') return tasks
+    return tasks.filter(task => task.priority === Filter)
+  }, [tasks, Filter])
+
   if (loading) return <div className="center">Loading...</div>
 
   return (
-    <div>
+        <div>
       <div className="page-header">
-        <h2>Your Tasks</h2>
-        <Link to="/tasks/add" className="btn">Add Task</Link>
+        <div className="page-header-left">
+          <h2>Your Tasks</h2>
+        </div>
+
+        <div className="page-header-right">
+          {/* Priority Filter */}
+          <select
+            className="priority-filter"
+            value={Filter}
+            onChange={(e) => setFilter(e.target.value)}
+          >
+            <option value="">Filter Results</option>
+
+            {/* <option value="">All</option> */}
+            <option value="Low">Low</option>
+            <option value="Medium">Medium</option>
+            <option value="High">High</option>
+          </select>
+
+          <Link to="/tasks/add" className="btn">Add Task</Link>
+        </div>
       </div>
 
-      {tasks.length === 0 ? (
+      {filteredTasks.length === 0 ? (
         <p>No tasks found.</p>
       ) : (
         <div className="tasks-grid">
-          {tasks.map(t => (
+          {filteredTasks.map(t => (
             <TaskCard key={t._id} task={t} onDelete={handleDelete} />
           ))}
         </div>
